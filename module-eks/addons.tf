82b0a565accaf76ea5c3f3365ac82bf2c39e1fb4
StanleyOluebube <stanleyoluebube171@gmail.com>
2026-05-17 16:33:49 -0500

    Updated the pipeline

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,5 +1,14 @@
+# provider "helm" {
+#   kubernetes = {          # ← assignment syntax with = sign
+#     host                   = module.eks.cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#   }
+# }
+
+
 provider "helm" {
-    kubernetes {
+    kubernetes = {
         host                   = aws_eks_cluster.eks.endpoint
         cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
         token                  = data.aws_eks_cluster_auth.eks.token
