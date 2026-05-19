4d44a94dc6cc4169e3938d193be6c9e8daa4a86d
Stanleyoluebube <stanleyoluebube171@gmail.com>
2026-05-19 15:00:59 -0500

    Updated the pipeline

--- backend.tf
+++ backend.tf
@@ -1,9 +1,12 @@
 terraform {
-  backend "s3" {
-    bucket         = "pod12-cohort5-digitalwitch"
-    key            = "bankapp/production/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+  required_version = "~> 1.15"
+
+  cloud {
+    
+    organization = "Pod12-cohort5-digitalwitch"
+
+    workspaces {
+      name = "BankApp-Project"
+    }
   }
 }
